53dc6cd75f27c0d3f6b1e2a75dc91bd95c972566
Clayton Hart <claytonhart123@yahoo.com>
2019-11-19 23:01:15 -0800

    initial app layout

---
+++ client/src/components/App/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TopBooks from 'components/TopBooks';
+
+function App() {
+  return (
+    <div className='App'>
+      <TopBooks />
+    </div>
+  );
+}
+
+export default App;
