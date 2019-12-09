c159f90306905d7c9cdca8acf95ac32e4ac74396
Clayton Hart <claytonhart123@yahoo.com>
2019-12-08 18:28:41 -0800

    initial pagination links

--- client/src/components/BookSearch/BookList/index.js
+++ client/src/components/BookSearch/BookList/index.js
@@ -4,6 +4,7 @@ import styled from 'styled-components/macro';
 const ListContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
+  margin: 20px 0;
 `;
 
 const BookList = ({ bookData }) => {
--- client/src/components/BookSearch/index.js
+++ client/src/components/BookSearch/index.js
@@ -17,15 +17,42 @@ const Header = styled.h2`
   font-size: 30px;
 `;
 
+const PaginationLinks = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-self: stretch;
+`;
+
+const Prev = styled.button`
+  background: ${props => props.theme.primary.lightblue};
+  color: #fff;
+  padding: 10px 20px;
+  cursor: pointer;
+`;
+const Next = styled.button`
+  background: ${props => props.theme.primary.lightblue};
+  color: #fff;
+  padding: 10px 20px;
+  cursor: pointer;
+`;
+
 const BookSearch = ({ bookList, getBookList }) => {
   useEffect(() => {
     getBookList();
   }, [getBookList]);
 
+  const getNextBookList = () => {
+    getBookList('Not harry potter');
+  };
+
   return (
     <Container>
       <Header>Top Books</Header>
       {bookList && <BookList bookData={bookList} />}
+      <PaginationLinks>
+        <Prev onClick={getNextBookList}>Previous</Prev>
+        <Next onClick={getNextBookList}>Next</Next>
+      </PaginationLinks>
     </Container>
   );
 };
